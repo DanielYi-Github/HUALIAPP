a505765fe9144056a7db21479b6abd66bca97b19
DanielYi <danyan602@gmail.com>
2021-01-27 18:23:10 +0800

    正式機 新增ios熱更新功能

--- js/utils/Contant.js
+++ js/utils/Contant.js
@@ -1,5 +1,5 @@
-// export const TOMCAT_HOST = 'http://app.hff-group.com/MobileApp/';		// 正式機
-export const TOMCAT_HOST = 'http://qasapp.hff-group.com/MobileApp/';		// 測試機
+export const TOMCAT_HOST = 'http://app.hff-group.com/MobileApp/';		// 正式機
+// export const TOMCAT_HOST = 'http://qasapp.hff-group.com/MobileApp/';		// 測試機
 // export const TOMCAT_HOST = 'http://10.0.0.116/MobileApp/';			    // 開發機
 // export const TOMCAT_HOST = 'http://10.0.96.250:8686/MobileApp/';			    // 開發機
 
@@ -8,9 +8,9 @@ export const AES_KEY = 'HFFGROUP20180313';
 export const AES_VI  = 'HFFGROUP20180313';
 
 // 正式機
-// export const CODE_PUSH_KEY_ANDROID = '9ai2v7OA6K1iZDUAtCuCTm91aR6Y4ksvOXqog';
-// export const CODE_PUSH_KEY_IOS     = 'pTMusJbFgikGtX7cgHKDGQl1JiYP4ksvOXqog';
+export const CODE_PUSH_KEY_ANDROID = '9ai2v7OA6K1iZDUAtCuCTm91aR6Y4ksvOXqog';
+export const CODE_PUSH_KEY_IOS     = 'pTMusJbFgikGtX7cgHKDGQl1JiYP4ksvOXqog';
 
 // 測試機
-export const CODE_PUSH_KEY_ANDROID = 'onpNlKOEeSLvZybeEKXPVQVHmc6O4ksvOXqog';
-export const CODE_PUSH_KEY_IOS = 'AJhlSYXl4E1Vhy1XxvQN6Q0IsDKl4ksvOXqog';
+// export const CODE_PUSH_KEY_ANDROID = 'onpNlKOEeSLvZybeEKXPVQVHmc6O4ksvOXqog';
+// export const CODE_PUSH_KEY_IOS = 'AJhlSYXl4E1Vhy1XxvQN6Q0IsDKl4ksvOXqog';
