f39e0a33c725741c3ecba41c0c83639f7f081604
yanzhuang.lin <1300235995@qq.com>
2021-06-02 09:03:35 +0800

    123

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './js/Root';
 // import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+console.log('sss');
