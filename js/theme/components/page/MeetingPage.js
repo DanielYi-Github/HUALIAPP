bb4d40edced85835ea313bb665b1d5f7e52adcda
DanielYi <danyan602@gmail.com>
2021-06-02 09:26:14 +0800

    測試機 新增會議預約功能(開發中)

---
+++ js/theme/components/page/MeetingPage.js
@@ -0,0 +1,9 @@
+import variable from "../../variables/platform";
+
+export default (variables /*: * */ = variable) => {
+  const meetingPageTheme = {
+    containerBgColor:variable.containerBgColor
+  };
+
+  return meetingPageTheme;
+};
