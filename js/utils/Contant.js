22a17b58ba139981309c675f20ff680ccb2ae2ea
DanielYi <danyan602@gmail.com>
2022-01-04 17:28:20 +0800

    測試機 新增網域路徑判斷

--- js/utils/Contant.js
+++ js/utils/Contant.js
@@ -1,4 +1,19 @@
-export const TOMCAT_HOST = 'http://app.huali-group.com:8080/MobileApp/';    		// 正式機
+import * as RNLocalize from "react-native-localize";
+
+let host = 'http://app.huali-group.com:8080/MobileApp/';
+console.log(RNLocalize.getTimeZone());
+if( RNLocalize.getTimeZone() == "Asia/Shanghai" ){
+	host = 'http://app.huali-group.com:8080/MobileApp/';
+	// host = 'http://qas.app.huali-group.com:8080/MobileApp/';
+	
+	console.log(123);
+}else{
+	host = 'http://qas.app.huali-group.com:8080/MobileApp/';
+	console.log(456);
+}
+
+export const TOMCAT_HOST = host ;
+// export const TOMCAT_HOST = 'http://app.huali-group.com:8080/MobileApp/';    		// 正式機
 // export const TOMCAT_HOST = 'http://qas.app.huali-group.com:8080/MobileApp/';		// 測試機
 // export const TOMCAT_HOST = 'http://10.0.0.116:8080/MobileApp/';					// 開發機
 
