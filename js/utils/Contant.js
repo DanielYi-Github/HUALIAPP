6d18850a294114698904d55128fcb8509d37d566
DanielYi <danyan602@gmail.com>
2022-01-05 11:11:48 +0800

    測試機 更新節點

--- js/utils/Contant.js
+++ js/utils/Contant.js
@@ -2,14 +2,18 @@ import * as RNLocalize from "react-native-localize";
 
 let host = 'http://app.huali-group.com:8080/MobileApp/';
 console.log(RNLocalize.getTimeZone());
+
 if( RNLocalize.getTimeZone() == "Asia/Shanghai" ){
-	host = 'http://app.huali-group.com:8080/MobileApp/';
-	// host = 'http://qas.app.huali-group.com:8080/MobileApp/';
-	
 	console.log(123);
+	// host = 'http://app.huali-group.com:8080/MobileApp/';    	// 正式機
+	host = 'http://10.0.0.113:8080/';							// 測試機
+	// host = 'http://10.0.0.116:8080/MobileApp/';				// 開發機
+	
 }else{
-	host = 'http://qas.app.huali-group.com:8080/MobileApp/';
 	console.log(456);
+	// host = 'http://app.huali-group.com:8080/MobileApp/';    // 正式機
+	host = 'http://qas.app.huali-group.com:8080/MobileApp/';// 測試機
+	// host = 'http://10.0.0.116:8080/MobileApp/';				// 開發機
 }
 
 export const TOMCAT_HOST = host ;
